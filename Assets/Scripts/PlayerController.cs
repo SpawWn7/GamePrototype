a30eb82c31d1eaf2463d00792f551565a12b50c0
Briant Anaya <briantanaya@yahoo.com>
2025-08-22 01:16:48 -0700

    Modified Script
    I modified the given script from the youtuber to suit my needs. It still needs work. It needs logic for the fish biting the bait. Animations work as well but need to properly be integrated with logic yet to be added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,98 +7,31 @@ using UnityEngine;
 using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
-    /*
-    private bool isCasted;
-    private Animator playerAnim;
-    
-    void Start()
-    {
-        playerAnim = GetComponent<Animator>();
-    }
-
-    
-    void Update()
-    {
-        if( Input.GetKeyDown(KeyCode.Space) && !isCasted ) 
-        {
-            isCasted = true;
-            //playerAnim.SetTrigger();
-        }
-
-    }
-    */
-    public bool isEquipped;
-    public bool isFishingAvailable;
+    private Vector3 baitPos = new Vector3(7.12f, 10.474f, -0.19f); // This will be the position that the bait spawns at
 
     public bool isCasted;
     public bool isPulling;
 
     Animator animator;
     public GameObject baitPrefab;
-    public GameObject endof_of_rope;  // --- > IF USING ROPE
-    public GameObject start_of_rope;   // --- > IF USING ROPE   
-    public GameObject start_of_rod;    // --- > IF USING ROPE   
-
     Transform baitPosition;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
-        isEquipped = true;
     }
 
 
 
     void Update()
     {
-        if (isEquipped)
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isCasted && !isPulling)
         {
-            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-            {
-
-                if (hit.collider.CompareTag("FishingArea"))
-                {
-                    isFishingAvailable = true;
-
-                    if (Input.GetMouseButtonDown(0) && !isCasted && !isPulling)
-                    {
-                        StartCoroutine(CastRod(hit.point));
-                    }
-                }
-                else
-                {
-                    isFishingAvailable = false;
-                }
-
-            }
-            else
-            {
-                isFishingAvailable = false;
-            }
+            StartCoroutine(CastRod(baitPos));
         }
 
-        // --- > IF USING ROPE < --- //
-        if (isCasted || isPulling)
-        {
-            if (start_of_rope != null && start_of_rod != null && endof_of_rope != null)
-            {
-                start_of_rope.transform.position = start_of_rod.transform.position;
-
-                if (baitPosition != null)
-                {
-                    endof_of_rope.transform.position = baitPosition.position;
-                }
-            }
-            else
-            {
-                Debug.Log("MISSING ROPE REFERENCES");
-            }
-        }
-
-        if (isCasted && Input.GetMouseButtonDown(1))
+        if (isCasted && Input.GetKeyDown(KeyCode.Space))
         {
             PullRod();
         }
@@ -115,6 +48,7 @@ public class PlayerController : MonoBehaviour
 
         GameObject instantiatedBait = Instantiate(baitPrefab);
         instantiatedBait.transform.position = targetPosition;
+        instantiatedBait.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
 
         baitPosition = instantiatedBait.transform;
 
